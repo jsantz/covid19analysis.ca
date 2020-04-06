77778bec2f82d7194028dba145d17a529e0e10d5
Josh S <j@jsantz.com>
2020-04-05 23:32:08 -0600

    Design7

--- aggregator.php
+++ aggregator.php
@@ -8,8 +8,8 @@
      *
      */
 
-    define( 'LOGPATH', '../wordpress/wp-content/uploads/plotterlogs' );
-    //define( 'LOGPATH', '../somerandom.co/wp-content/uploads/plotterlogs' );
+    //define( 'LOGPATH', '../wordpress/wp-content/uploads/plotterlogs' );
+    define( 'LOGPATH', '../somerandom.co/wp-content/uploads/plotterlogs' );
     // MAIN
 
     $worldOmeters = array();
