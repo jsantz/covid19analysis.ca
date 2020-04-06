0d204b7acf927bdf046e9c17820b0aab242850eb
Josh S <j@jsantz.com>
2020-04-05 22:49:10 -0600

    Added Model

---
+++ model/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<meta charset="utf-8">
+<title>Pandemic Model</title>
+<body>
+<script src="../js/pixi.min.js"></script>
+<script src="../js/model.min.js"></script>
+<script>
+</script>
+</body>
+
+
