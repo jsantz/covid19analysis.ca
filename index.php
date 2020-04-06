9bcb9c2db07b33c33418f839a2b858e452e25b5d
Josh S <j@jsantz.com>
2020-04-05 23:04:39 -0600

    changing site format

--- index.php
+++ index.php
@@ -5,30 +5,9 @@
 * Date: March 22, 2019
 *
 */
+include 'header.php';
 ?>
 
-<!doctype html>
-<html>
-<head>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://momentjs.com/downloads/moment-with-locales.js"></script>
-    <script src="js/Chart.min.js"></script>
-    <title>COVID19</title>
-    <link rel="shortcut icon" type="image/x-icon" href="https://argylefoxinc.com/functions/modules/argylefox/images/favicon.ico" />
-
-    <style>
-        body {font: 10pt arial;}
-    </style>
-
-    <script type="text/javascript" src="js/vis-graph3d.min.js"></script>
-
-    <script type="text/javascript">
-    </script>
-    <!--script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script-->
-</head>
 
 <body>
 <h1 class="display-4"><span class="text-danger">COVID-19</span> Data Tracking/Analysis</h1>
