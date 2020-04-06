52663ce102620ad1957260cee84ba4a900d8e633
Josh S <j@jsantz.com>
2020-04-06 05:50:15 -0600

    Design14

--- model/index.php
+++ model/index.php
@@ -11,43 +11,43 @@ include '../header.php';
         text-align: center;
         line-height: 1.6em;
     }
+    #transmissibility-handle {
+        width: 5em;
+        height: 3em;
+        top: 50%;
+        margin-top: -.8em;
+        text-align: center;
+        line-height: 1.6em;
+    }
 </style>
 <body>
 <script src="../js/Chart.min.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <button id="runsimulation" class="btn btn-outline-primary btn-lg">RESET SIMULATION</button>
-<p class="h4">
+<hr>
+
+
+<p class="h6">
     <label for="amount">Total Healthy Population</label>
-    <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">| (double tap slidebar to move)
+    <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">| (double tap slidebars to set values)
 </p>
 <div id="slider-range-min">
     <div id="custom-handle" class="ui-slider-handle"></div>
 </div>
-<div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+<hr>
+
+
+<p class="h6">
+    <label for="transmissibility">Transmissibility</label>
+    <input type="text" id="transmissibility-label" readonly style="border:0; color:#f6931f; font-weight:bold;">
+</p>
+<div id="transmissibility">
+    <div id="transmissibility-handle" class="ui-slider-handle"></div>
 </div>
-<div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
-</div>
-<div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
-</div>
-<div class="form-group">
+<hr>
+
+
+
 <script src="../js/pixi.min.js"></script>
 <script src="../js/model.min.js"></script>
     <div style="width:700px;">
@@ -60,7 +60,7 @@ include '../header.php';
                 range: "min",
                 value: 200,
                 min: 50,
-                max: 300,
+                max: 400,
                 create: function() {
                     handle.text( $( this ).slider( "value" ) );
                 },
@@ -70,6 +70,23 @@ include '../header.php';
                 }
             });
             $( "#amount" ).val(  + $( "#slider-range-min" ).slider( "value" ) );
+        } );
+        $( function() {
+            var handle = $( "#transmissibility-handle" );
+            $( "#transmissibility" ).slider({
+                range: "min",
+                value: 20,
+                min: 1,
+                max: 100,
+                create: function() {
+                    handle.text( $( this ).slider( "value" ) );
+                },
+                slide: function( event, ui ) {
+                    $( "#transmissibility-label" ).val('% ' + ui.value );
+                    handle.text('%' + ui.value );
+                }
+            });
+            $( "#transmissibility-label" ).val('% '  + $( "#transmissibility" ).slider( "value" ) );
         } );
     </script>
 </body>
