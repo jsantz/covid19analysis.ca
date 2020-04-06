b2bbf61722d13b31bf316b9ba7771386988dc0a0
Josh S <j@jsantz.com>
2020-04-06 04:51:33 -0600

    Design11

--- model/index.php
+++ model/index.php
@@ -2,6 +2,16 @@
 <?php
 include '../header.php';
 ?>
+<style>
+    #custom-handle {
+        width: 5em;
+        height: 3em;
+        top: 50%;
+        margin-top: -.8em;
+        text-align: center;
+        line-height: 1.6em;
+    }
+</style>
 <body>
 <script src="../js/Chart.min.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -10,7 +20,9 @@ include '../header.php';
     <label for="amount">Total Healthy Population</label>
     <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
 </p>
-<div id="slider-range-min"></div>
+<div id="slider-range-min">
+    <div id="custom-handle" class="ui-slider-handle"></div>
+</div>
 <div class="form-group">
     <label for="exampleFormControlInput1">Email address</label>
     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
@@ -43,13 +55,18 @@ include '../header.php';
     </div>
     <script>
         $( function() {
+            var handle = $( "#custom-handle" );
             $( "#slider-range-min" ).slider({
                 range: "min",
                 value: 200,
                 min: 50,
                 max: 300,
+                create: function() {
+                    handle.text( $( this ).slider( "value" ) );
+                },
                 slide: function( event, ui ) {
-                    $( "#amount" ).val( "$" + ui.value );
+                    $( "#amount" ).val( ui.value );
+                    handle.text( ui.value );
                 }
             });
             $( "#amount" ).val(  + $( "#slider-range-min" ).slider( "value" ) );
